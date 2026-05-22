d3adbb1a3887773aee6a8a06b888112812536d29
NinjaskillzX <oliverbostrom@icloud.com>
2026-05-22 22:59:47 +0200

    Add files via upload

---
+++ stuff.js
@@ -0,0 +1,14 @@
+/* Jag ger upp jag har suttit här i typ 6 timmar, plus flera andra dagar, att försöka med javascript men det går inte. Jag orkar bara inte. Allt vill bara inte fungera både det svåra och lätta vill inte fungera och när jag försöker lägga till något nytt går allt annant bara sönder. Så just nu klockan 21:03 har jag gett upp och nöjer mig med ett E i den här kursen för att jag inte kunde göra något i javascript. Tack och good natt syns när vi syns. */ 
+
+//const toTop = () => window.scrollTo({top: 0, behavior: 'smooth'});
+
+/*const toTop = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+};
+
+document
+    .getElementById("hej-nej")
+    .addEventListener("click", toTop);*/
